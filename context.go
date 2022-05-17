b441474b01a8eaa5de5569aaf3bd70c1f050c694
ppcamp <p.augustocampos@gmail.com>
2022-05-16 22:20:47 -0300

    refactor: rename and add doc to context block method

---
+++ context.go
@@ -0,0 +1,37 @@
+package timed
+
+import (
+	"context"
+)
+
+// ForceContextClose will ensure that, whether finish first (context close, or function finishes),
+// stop the execution.
+//
+// Note
+//
+// With this approach, if the context are cancelled, you're state will be unkown or corrupted.
+// Remember that, if you wanna make sure that the application guarantee the state, you must use
+// just a for loop within a select block.
+//
+// Basic example of using this decorator
+// 	ctx, cancel := context.WithTimeout(context.Background(), 3 * time.Second)
+//	defer cancel()
+//
+// 	err := ForceContextClose(ctx, func() error {
+//		time.Sleep(4 * time.Second)
+//		return nil
+//	})
+//	if err != nil {
+//		print("context canceled, therefore, the block state will be unknown")
+//	}
+func ForceContextClose(ctx context.Context, fn func() error) error {
+	err := make(chan error, 1)
+	go func() { err <- fn() }()
+
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case e := <-err:
+		return e
+	}
+}
