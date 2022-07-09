c0a20c5e835dbf573ec8768fd0e6ed11c35640c8
Pedro Santos <p.augustocampos@gmail.com>
2022-07-09 12:40:00 -0300

    fix: strings pkg when string is empty

--- strings/int.go
+++ strings/int.go
@@ -3,6 +3,10 @@ package strings
 import "strconv"
 
 func ToInt[T int | int32 | int64](v string) (T, error) {
+	if v == "" {
+		v = "0"
+	}
+
 	a, err := strconv.Atoi(v)
 	if err != nil {
 		var r T
